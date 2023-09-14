4c9d65184c4770a0b2d2e0ca45ff6db9d983e70d
elkake <gianco.marquez@gmail.com>
2023-09-14 20:41:24 -0300

    probando mobile 1

--- src/helpers/LazyLoad.jsx
+++ src/helpers/LazyLoad.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 import { useEffect, useRef } from 'react'
 
-function LazyLoad(disconnect = true, options){
+function LazyLoad(disconnect = true, options) {
   const domRef = useRef(null) // Referencia para el elemento DOM
-  const [visible , setVisible] = useState(false) // Referencia para el estado de intersección
+  const [visible, setVisible] = useState(false) // Referencia para el estado de intersección
 
   useEffect(() => {
     // Creamos el observador de intersección
+    if (window.innerWidth < 1200) {
+      setVisible(true)
+      return
+    }
+
     const observer = new IntersectionObserver((entries) => {
       const isIntersecting = entries[0].isIntersecting
-      setVisible(isIntersecting) 
+      setVisible(isIntersecting)
       if (visible && disconnect) {
-        observer.disconnect() 
+        observer.disconnect()
       }
     }, options)
 
-    if (domRef.current) {
+    if (domRef.current && window.innerWidth > 1200) {
       observer.observe(domRef.current)
     }
 
